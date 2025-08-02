44c670c65f5ee1077db954f6ae4eaab5add11eaf
Jiman Kim <iojjdwn@gmail.com>
2025-08-02 21:39:57 +0900

    login commit

--- models/User.js
+++ models/User.js
@@ -1,40 +1,50 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 // 스키마 만들기
-const userSchema = Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: String,
+      default: "costomer", // 권한은 admin/costmer 2개
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  level: {
-    type: String,
-    default: "costomer", // 권한은 admin/costmer 2개
-  },
-}, {timestamps:true});
-
+  { timestamps: true }
+);
 
 // 불필요 정보 제거
 userSchema.methods.toJSON = function () {
-    const obj = this._doc
-    delete obj.password
-    delete obj.__v
-    delete obj.updateAt
-    delete obj.createAt
-    return obj
-}
+  const obj = this._doc;
+  delete obj.password;
+  delete obj.__v;
+  delete obj.updateAt;
+  delete obj.createAt;
+  return obj;
+};
 
+userSchema.methods.generateToken = async function () {
+  const token = await jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
-
