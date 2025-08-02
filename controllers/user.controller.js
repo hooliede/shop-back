b3a7d3eb854a44e504da54c5fb48539e02d3aa46
Jiman Kim <iojjdwn@gmail.com>
2025-08-02 15:13:06 +0900

    register commit

---
+++ controllers/user.controller.js
@@ -0,0 +1,28 @@
+const bcryptjs = require("bcryptjs");
+const User = require("../models/User");
+
+const userController = {};
+
+userController.createUser = async (req, res) => {
+  try {
+    const { name, password, email, level } = req.body;
+    const user = await User.findOne({ email: email });
+    if(user){
+      throw new Error("User already exist");
+    }
+    const salt = await bcryptjs.genSaltSync(10);
+    const hash = await bcryptjs.hash(password, salt);
+    const newUser = new User({
+      email,
+      password: hash,
+      name,
+      level: level ? level : "customer",
+    });
+    await newUser.save();
+    res.status(200).json({ status: "success" });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+module.exports = userController;
